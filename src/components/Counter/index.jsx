5f6e7453ff7c2d5010b26ed7552b3de7a370f293
ivanmla <ivan0mlakic@gmail.com>
2023-03-22 00:49:50 +0100

    ponavljanje i zadan zadatak dopuna

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -7,9 +7,6 @@ const Counter = () => {
         aa: 111,
         bb: 222
     })
-    let ime = "Matej" 
-    
-    console.log(ime)
 
     const sveHandler = () => {
         setCount(count => count + 1) // setCount se ponaša kao asinkron, ali nije ??
@@ -17,15 +14,12 @@ const Counter = () => {
         console.log(count)
         // koristit će se spread operator, npr: 
         setObj({...obj, bb: "nestodrugo"})
-        ime = "Algebra"// ako koristimo let ime = "Matej" svaki put će biti Matej, jer setState će ponovo crtati komponentu i pročitaiti matej
-        // dok ako koristimo setState, samo ćemo crtati stablo, nećemo mojenjeti varijablu
         
     }
     return (
         <>
             <h2>{ count }</h2>
             <div>aa = {obj.aa}, a bb = {obj.bb}</div>
-            <h3>{ ime }</h3>
             <button onClick={sveHandler}>Add</button>
         </>
     )
